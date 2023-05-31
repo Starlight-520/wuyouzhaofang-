c6259e5a6b91e0ba0e6e73ef10011a3a08a4ff97
张俊龙 <384686079@qq.com>
2023-05-31 10:16:57 +0800

    首页全部完成

--- ZJL/js/1.js
+++ ZJL/js/1.js
@@ -6,3 +6,10 @@ function rem() {
 }
 window.onload = rem
 window.onresize = rem
+// 透明盒子点击事件
+var img_ = document.getElementsByClassName('close')
+var hezi = document.getElementsByClassName('dr')
+function arr() {
+    hezi[0].style.display = 'none'
+
+}
